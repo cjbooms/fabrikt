ebe67b68437ddd13711f4e15d28b44da53bdb0cb
Conor Gallagher <conor.gallagher@zalando.ie>
2021-01-26 23:52:15 +0000

    Improving support for external references from an API document. Previously generation stopped at an array

--- src/test/resources/examples/externalReferences/models/Models.kt
+++ src/test/resources/examples/externalReferences/models/Models.kt
@@ -1,8 +1,11 @@
 package examples.externalReferences.models
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 import javax.validation.Valid
+import javax.validation.constraints.NotNull
 import kotlin.String
+import kotlin.collections.List
 
 data class ContainingExternalReference(
     @param:JsonProperty("some-external-reference")
@@ -11,8 +14,38 @@ data class ContainingExternalReference(
     val someExternalReference: ExternalObject? = null
 )
 
+enum class Enum(
+    @JsonValue
+    val value: String
+) {
+    ONE("one"),
+
+    TWO("two"),
+
+    THREE("three");
+}
+
+data class ExternalObjectThree(
+    @param:JsonProperty("enum")
+    @get:JsonProperty("enum")
+    @get:NotNull
+    val enum: Enum,
+    @param:JsonProperty("description")
+    @get:JsonProperty("description")
+    @get:NotNull
+    val description: String
+)
+
+data class ExternalObjectTwo(
+    @param:JsonProperty("list-others")
+    @get:JsonProperty("list-others")
+    @get:Valid
+    val listOthers: List<ExternalObjectThree>? = null
+)
+
 data class ExternalObject(
-    @param:JsonProperty("some-string")
-    @get:JsonProperty("some-string")
-    val someString: String? = null
+    @param:JsonProperty("another")
+    @get:JsonProperty("another")
+    @get:Valid
+    val another: ExternalObjectTwo? = null
 )
