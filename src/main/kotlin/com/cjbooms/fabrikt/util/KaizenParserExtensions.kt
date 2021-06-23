cc742ea35ad00c3a9848f8b5454a7da2d0f58f0a
Conor Gallagher <conor.gallagher@zalando.ie>
2021-06-23 10:57:55 +0100

    Add parameter name for readability

--- src/main/kotlin/com/cjbooms/fabrikt/util/KaizenParserExtensions.kt
+++ src/main/kotlin/com/cjbooms/fabrikt/util/KaizenParserExtensions.kt
@@ -67,7 +67,7 @@ object KaizenParserExtensions {
         )
 
     fun Schema.isUuidDefinition(): Boolean =
-        this.type == OasType.Text.type && (this.format?.equals("uuid", true) == true)
+        this.type == OasType.Text.type && (this.format?.equals("uuid", ignoreCase = true) == true)
 
     @Suppress("UNCHECKED_CAST")
     fun Schema.getEnumValues(): List<String> = when {
