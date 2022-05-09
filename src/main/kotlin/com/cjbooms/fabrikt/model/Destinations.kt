7b03c18cd8b85f0aac56f54cdee9ee1c151d6af4
Conor Gallagher <conor.gallagher@zalando.ie>
2022-05-09 12:56:52 +0100

    Fix relative paths

--- src/main/kotlin/com/cjbooms/fabrikt/model/Destinations.kt
+++ src/main/kotlin/com/cjbooms/fabrikt/model/Destinations.kt
@@ -1,11 +1,13 @@
 package com.cjbooms.fabrikt.model
 
 import java.nio.file.Path
+import java.nio.file.Paths
 
 object Destinations {
 
-    val MAIN_KT_SOURCE: Path = Path.of("/src/main/kotlin/")
-    val MAIN_RESOURCES: Path = Path.of("/src/main/resources")
+    private val PATH_ROOT: Path = Paths.get("")
+    val MAIN_KT_SOURCE: Path = PATH_ROOT.resolve("src/main/kotlin/")
+    val MAIN_RESOURCES: Path = PATH_ROOT.resolve("src/main/resources")
 
     fun modelsPackage(basePackage: String): String = "$basePackage.models"
     fun controllersPackage(basePackage: String): String = "$basePackage.controllers"
