4442a59abe9f09e8a37a2f700eb298bb802ed012
Conor Gallagher <conor.gallagher@zalando.ie>
2023-07-18 20:57:11 +0100

    Removed dedicated test case by adding issue-223 examples into another test case

--- src/test/kotlin/com/cjbooms/fabrikt/generators/ModelGeneratorTest.kt
+++ src/test/kotlin/com/cjbooms/fabrikt/generators/ModelGeneratorTest.kt
@@ -62,8 +62,8 @@ class ModelGeneratorTest {
         )
     }
 
-    @Test
-    fun `debug single test`() = `correct models are generated for different OpenApi Specifications`("issue223")
+    // @Test
+    // fun `debug single test`() = `correct models are generated for different OpenApi Specifications`("insert test case")
 
     @ParameterizedTest
     @MethodSource("testCases")
