e17da20f55f964fb518c9ddf1f65e573f509e57a
DavidMazarro <davidmazarro98@gmail.com>
2024-06-20 18:11:23 +0200

    test: Updated `NullableArrayItems` to be nullable

--- src/test/resources/examples/nullableArrayItems/models/NullableArrayItems.kt
+++ src/test/resources/examples/nullableArrayItems/models/NullableArrayItems.kt
@@ -1,13 +1,11 @@
 package examples.nullableArrayItems.models
 
 import com.fasterxml.jackson.`annotation`.JsonProperty
-import javax.validation.constraints.NotNull
 import kotlin.String
 import kotlin.collections.List
 
 public data class NullableArrayItems(
   @param:JsonProperty("anArray")
   @get:JsonProperty("anArray")
-  @get:NotNull
-  public val anArray: List<String?>,
+  public val anArray: List<String?>?,
 )
