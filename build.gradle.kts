d22cbc130f004d3c3e7a2561f3d06bb8e0886d21
Conor Gallagher <conor.gallagher@zalando.ie>
2024-07-05 17:32:15 +0100

    Update build.gradle.kts
    changing to user token

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,8 +135,8 @@ publishing {
             name = "OSSRH"
             url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
             credentials {
-                username = System.getenv("OSSRH_USERNAME")
-                password = System.getenv("OSSRH_PASSWORD")
+                username = System.getenv("OSSRH_USER_TOKEN_USERNAME")
+                password = System.getenv("OSSRH_USER_TOKEN_PASSWORD")
             }
         }
     }
