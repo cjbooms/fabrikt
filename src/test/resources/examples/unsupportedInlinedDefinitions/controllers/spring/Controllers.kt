5172998147708e88f5bb03bc7dfe04aec6f48b2b
Conor Gallagher <conor.gallagher@zalando.ie>
2025-01-29 13:47:45 +0000

    Minor improvement to handle String enums as Strings.

--- src/test/resources/examples/unsupportedInlinedDefinitions/controllers/spring/Controllers.kt
+++ src/test/resources/examples/unsupportedInlinedDefinitions/controllers/spring/Controllers.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.`annotation`.RequestMapping
 import org.springframework.web.bind.`annotation`.RequestMethod
 import org.springframework.web.bind.`annotation`.RequestParam
 import kotlin.Any
+import kotlin.String
 import kotlin.collections.List
 
 @Controller
@@ -25,7 +26,7 @@ public interface ExampleController {
         method = [RequestMethod.GET],
     )
     public fun `get`(
-        @RequestParam(value = "inline_enum.", required = false) inlineEnum: Any?,
+        @RequestParam(value = "inline_enum.", required = false) inlineEnum: String?,
         @RequestParam(value = "inline_obj.", required = false) inlineObj: Any?,
     ): ResponseEntity<List<Any>>
 }
