820b1c6117278e676e168fcd5ac1d83726063d0b
Conor Gallagher <conor.gallagher@zalando.ie>
2025-09-09 22:53:03 +0100

    fix deprecation warning

--- src/test/kotlin/com/cjbooms/fabrikt/util/Linter.kt
+++ src/test/kotlin/com/cjbooms/fabrikt/util/Linter.kt
@@ -3,17 +3,18 @@ package com.cjbooms.fabrikt.util
 import com.pinterest.ktlint.rule.engine.api.Code
 import com.pinterest.ktlint.rule.engine.api.KtLintRuleEngine
 import com.pinterest.ktlint.ruleset.standard.StandardRuleSetProvider
+import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
 
 object Linter {
 
-    val shouldLint = true
     fun lintString(rawText: String): String {
-        return if (shouldLint) {
-            val code = Code.fromSnippet(rawText)
-            val result = KtLintRuleEngine(
-                ruleProviders = StandardRuleSetProvider().getRuleProviders(),
-            ).format(code)
-            result
-        } else rawText
+        val code = Code.fromSnippet(rawText)
+        val result = KtLintRuleEngine(
+            ruleProviders = StandardRuleSetProvider().getRuleProviders(),
+        ).format(
+            code,
+            rerunAfterAutocorrect = true,
+            callback = { _ -> AutocorrectDecision.ALLOW_AUTOCORRECT })
+        return result
     }
 }
