d734a547dae4937043f45057d91ec3046135c8d6
Ulrik Andersen <ulrik@shape.dk>
2025-10-06 19:58:34 +0200

    Add end2end for Ktor client w/ Jackson

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ include(
     "end2end-tests:openfeign",
     "end2end-tests:ktor",
     "end2end-tests:ktor-client-kotlinx",
+    "end2end-tests:ktor-client-jackson",
     "end2end-tests:spring-http-interface",
     "end2end-tests:models-jackson",
     "end2end-tests:models-kotlinx",
